e88196d809814668006e0992f96d60caf156f164
Abdullah <mohamanobabdullah@gmail.com>
2023-05-28 19:41:19 +0000

    major overhaul, added bunch of new functionality

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const displayUpdate = (value) => {
 const clearDisplay = () => {
     return display.value = "";
 };
+const removeLastCharacter = () => {
+    const currentValue = display.value;
+    display.value = currentValue.slice(0, -1);
+}
 const giveResult = () => {
     return display.value = eval(display.value);
 };
